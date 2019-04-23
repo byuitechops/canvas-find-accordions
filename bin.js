f87e424d687b29c614345844a1c646bea78d893e
Cal Wilson <kilakal@byui.edu>
2019-04-23 12:00:17 -0600

    All Template files added

---
+++ bin.js
@@ -0,0 +1,31 @@
+const {
+    main
+} = require('./main.js');
+
+async function getInput() {
+    var input;
+    // How to get input, eg. from file, commandline, inquierer, etc.
+    return input;
+}
+
+async function makeOutput(output) {
+    // How to output data, eg. to csv, to json, to console, etc.
+    return output;
+}
+
+function handleError(error) {
+    console.error(error);
+    return;
+}
+
+async function start() {
+    try {
+        var input = await getInput();
+        var output = await main(input);
+        await makeOutput(output);
+    } catch (error) {
+        handleError(error);
+    }
+}
+
+start();
